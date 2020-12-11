49650cf5ab52b03c13c90fb5498f6897ac2d3210
zfinnan <63361320+zfinnan@users.noreply.github.com>
2020-12-11 11:11:11 -0500

    add browser routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+// third party
+import { BrowserRouter, Route } from 'react-router-dom'
+
+// your own components
+import Home from './Components/Home';
+import Contact from './Components/Contact';
+import Procedures from './Components/Procedures';
+
+// style
 import './App.css';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route path="/" exact component={Home} />
+      <Route path="/contact" component={Contact} />
+      <Route path="/procedures" component={Procedures} />
     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Components/Procedures.js
@@ -0,0 +1,5 @@
+function Procedures() {
+    return (<div>This is the Procedures page</div>)
+}
+
+export default Procedures
